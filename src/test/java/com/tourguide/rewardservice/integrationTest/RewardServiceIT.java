61ff5c9b963104127067f8bac8d929af4caff80e
loic <shin.loicb@gmail.Com>
2022-05-04 22:07:29 +0200

    Added Mock for feignclient call in IT.

--- src/test/java/com/tourguide/rewardservice/integrationTest/RewardServiceIT.java
+++ src/test/java/com/tourguide/rewardservice/integrationTest/RewardServiceIT.java
@@ -1,7 +1,10 @@
 package com.tourguide.rewardservice.integrationTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tourguide.rewardservice.client.LocationClient;
 import com.tourguide.rewardservice.exception.DataAlreadyExistException;
+import com.tourguide.rewardservice.exception.IllegalArgumentException;
+import com.tourguide.rewardservice.model.AttractionWithDistanceDto;
 import com.tourguide.rewardservice.model.Location;
 import com.tourguide.rewardservice.model.UserReward;
 import com.tourguide.rewardservice.model.VisitedLocation;
@@ -13,12 +16,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,6 +41,7 @@ public class RewardServiceIT {
   @Autowired MockMvc mockMvc;
   @Autowired RewardRepository rewardRepository;
   @Autowired ObjectMapper objectMapper;
+  @MockBean LocationClient locationClientMock;
 
   @Test
   void getUserRewards() throws Exception {
@@ -78,6 +85,15 @@ public class RewardServiceIT {
   @Test
   void addUserReward() throws Exception {
 
+    // mock client response
+    AttractionWithDistanceDto clientResponse =
+        new AttractionWithDistanceDto("Legend Valley", "Thornville", "OH", 39.937778, -82.40667, 5);
+    AttractionWithDistanceDto clientResponseTooFar =
+        new AttractionWithDistanceDto("testName", "Thornville", "OH", 39.937778, -82.40667, 15);
+    clientResponse.setAttractionId(UUID.randomUUID());
+    clientResponseTooFar.setAttractionId(UUID.randomUUID());
+    when(locationClientMock.getClosestAttraction(anyDouble(), anyDouble()))
+        .thenReturn(clientResponse, clientResponse, clientResponseTooFar);
     UUID userId = UUID.fromString("00000000-0000-0000-0000-000000000003");
     Date date = new Date();
     //    ("Legend Valley", "Thornville", "OH", 39.937778, -82.40667)
@@ -126,5 +142,17 @@ public class RewardServiceIT {
             result ->
                 assertTrue(
                     result.getResolvedException() instanceof MethodArgumentNotValidException));
+    // when attraction is too far
+    mockMvc
+        .perform(
+            post(addUserRewardUrl)
+                .param("userId", userId.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+        .andExpect(status().isBadRequest())
+        .andExpect(
+            result ->
+                assertTrue(
+                    result.getResolvedException() instanceof IllegalArgumentException));
   }
 }
